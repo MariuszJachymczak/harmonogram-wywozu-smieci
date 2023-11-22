afceaeb9dd270004e26abcdb32abefffbd6bd494
mariusz.jachymczak <mariusz.jachymczak@gazeta.pl>
2023-11-22 20:58:41 +0100

    cleaning up a little bit

---
+++ src/data/schedule.ts
@@ -0,0 +1,14 @@
+import { ScheduleType } from "./types";
+
+export const schedule: ScheduleType = {
+  listopad: {
+    Plastik: ["5", "19"],
+    Makulatura: ["12"],
+    Szklo: ["31"],
+  },
+  grudzien: {
+    Plastik: ["5", "19"],
+    Makulatura: ["12"],
+    Szklo: ["30"],
+  },
+};
